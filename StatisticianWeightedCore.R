2dd921cd604ddbdfb5027f8727649106c4c18a9e
Ari Jordan <ari.jordan@gmx.ch>
2021-04-30 15:51:35 +0200

    remove useless code

--- StatisticianWeightedCore.R
+++ StatisticianWeightedCore.R
@@ -43,7 +43,7 @@ PSVD <- irlba(W,nv=100)
 
 plot(1:100,PSVD$d)
 
-# Stability selection, (Meinhausen & Peter Bühlmann, 2010)
+# Stability selection, (Meinshausen & Peter Bühlmann, 2010)
 
 
 result <- foreach(k = 1:100, .packages='irlba')%dopar%{
@@ -70,7 +70,7 @@ h.seq <- seq(0,2,by=0.1)
 source("RandomHoldout-FinalSP_TestWeight.R")
 set.seed(500)
 #system.time(tune <- EdgeCV.REG.DC.Weight(W,h.seq,K=K,B=30,holdout.p=0.1,Arash=TRUE,fast=TRUE))
-#saveRDS(tune, file = "tune.rds")
+#saveRDS(tune, file = "tune.Rda")
 tune <- readRDS("tune.Rda")
 
 names(tune)
@@ -78,10 +78,6 @@ names(tune)
 tune$gap.which.min
 tune$gap.min.stable
 tune$gap.min.avg
-
-
-
-
 
 
 d <- colSums(W)
@@ -120,31 +116,7 @@ for(k in 1:K){
 }
 
 #save(km1,SSE.K,tune,file="CitationTune.Rda")
-
-load("CitationTune.Rda")
-
-weighted.cluster1[[1]]  ## regression and dimensionality reduction
-weighted.cluster1[[2]]  ## biostatistics - medical and genomics
-weighted.cluster1[[3]]  ## High dimensional statistics-covariance estimation
-weighted.cluster1[[4]]  ## Bayesian statistics
-weighted.cluster1[[5]] ## decision theory and wavelets
-weighted.cluster1[[6]] ## semi-parametric or nonparametrics modeling
-weighted.cluster1[[7]] ## Bayesian nonparametrics
-weighted.cluster1[[8]] ## Biostatistics and machine learning
-weighted.cluster1[[9]] ## high dimensional statistics
-weighted.cluster1[[10]] ## financial statistics
-weighted.cluster1[[11]] ## unclear
-weighted.cluster1[[12]] ## semi-parmatric modeling
-weighted.cluster1[[13]] ## high dimensional statistics
-weighted.cluster1[[14]] ## high dimensional statistics
-weighted.cluster1[[15]] ## multiple testing and inference
-weighted.cluster1[[16]] ## Bayesian methods and machine learning
-weighted.cluster1[[17]] ## high dimensional statistics
-weighted.cluster1[[18]] ## Geo-statistics and bayesian methods
-weighted.cluster1[[19]] ## biostatistics
-weighted.cluster1[[20]] ## functional data analysis
-
-
+#load("CitationTune.Rda")
 
 df <- rep("",K)
 for(k in 1:K){
@@ -155,106 +127,3 @@ df <- data.frame(authors=df)
 
 library(xtable)
 print(xtable(df))
-
-
-
-df <- rep("",K)
-for(k in 1:K){
-    df[k] <- paste(weighted.cluster1[[k]][,1],collapse=", ")
-}
-
-df <- data.frame(authors=df)
-
-library(xtable)
-print(xtable(df))
-
-
-
-
-
-
-d <- colSums(W)
-W.reg <- W
-
-d.reg <- colSums(W.reg)
-
-L0 <- t(t(W.reg/sqrt(d.reg))/sqrt(d.reg))
-
-
-PSVD <- irlba(L0,nv=20,nu=20)
-
-U <- PSVD$u
-
-dim(U)
-
-summary(colSums(W))
-
-
-norms <- apply(U,1,function(x)sqrt(sum(x^2)))
-
-U.norm <- U/norms
-
-set.seed(500)
-km0 <- kmeans(U.norm,centers=20,iter.max=500,nstart=500)
-
-
-weighted.label0 <- km0$cluster
-weighted.cluster0 <- list()
-for(k in 1:20){
-    tmp.authors <- authors[weighted.label0==k]
-    tmp.degrees <- d[weighted.label0==k]
-    tmp.index <- sort(tmp.degrees,decreasing=TRUE,index.return=TRUE)$ix
-    
-    weighted.cluster0[[k]] <- cbind(tmp.authors[tmp.index],tmp.degrees[tmp.index])
-}
-
-
-
-weighted.cluster0[[1]]  ## biostatistics
-weighted.cluster0[[2]]  ## regression and dimensionality reduction
-weighted.cluster0[[3]]  ## high dimensional statistics
-weighted.cluster0[[4]]  ## high dimensional statistics
-weighted.cluster0[[5]] ## Bayesian statistics
-weighted.cluster0[[6]] ## decision theory and wavelets
-weighted.cluster0[[7]] ## nonparametric and semiparametric modeling
-weighted.cluster0[[8]] ## Biostatistics -
-weighted.cluster0[[9]] ## Bayesian statistics and machine learning
-weighted.cluster0[[10]] ## Bayesian nonparametrics
-weighted.cluster0[[11]] ## financial statistics
-weighted.cluster0[[12]] ## Geo-statistics and bayesian methods
-weighted.cluster0[[13]] ## unclear
-weighted.cluster0[[14]] ## high dimensional statistics
-weighted.cluster0[[15]] ## Bayesian statistics and MCMC
-weighted.cluster0[[16]] ## Bayesian methods and machine learning
-weighted.cluster0[[17]] ## multiple testing and inference
-weighted.cluster0[[18]] ## functional data analysis, non-parametric/semi-parametric modeling
-weighted.cluster0[[19]] ## unclear
-weighted.cluster0[[20]] ## unclear
-
-
-
-
-
-ss <- load("StatisticianWeightedCore-Result.Rda")
-
-
-
-
-
-
-
-pdf("AuthorWeightNet15Core_withName.pdf",height=12,width=12)
-plot(g,layout=author.layout,edge.width=0.8)
-dev.off()
-
-
-
-
-pdf("AuthorWeightNet15Core_withName_noMargin.pdf",height=12,width=12)
-plot(g,layout=author.layout,edge.width=0.8,margin=c(0,0,0,0))
-dev.off()
-
-
-
-
-authors[hub.index]
