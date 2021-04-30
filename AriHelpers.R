8d49815034590210f50d317c840987ce6e4f3633
Ari Jordan <ari.jordan@gmx.ch>
2021-04-30 19:53:38 +0200

    Plot clusters in colors

--- AriHelpers.R
+++ AriHelpers.R
@@ -1,39 +1,31 @@
+require(doParallel)
+registerDoParallel(cores=6)
+
+source("LeiFunc-Final.R")
+source("SimonFunk.R")
+source("RandomHoldout.R")
+source("ArashGen.R")
+source("RandomHoldout-FinalSP_TestWeight.R")
 
 # Plot a weighted network in red-yellow
 plotNetwork <- function(W, remove=0){
-  set.seed(5) # 5
-  dim(W)
-  
   n <- nrow(W)
-  
-  
-  #A <- matrix(0,n,n)
   
   #A[as.matrix(W)>0] <- 1
   A <- as.matrix(W)
   diag(A) <- 0
   
   library(igraph)
-  
   g <- graph.adjacency(A,mode="undirected")
+  
   E(g)$weight <- 1
   g <- simplify(g)
   
-  library(RColorBrewer)
-  
-  colors <- colorRampPalette(brewer.pal(9,"YlOrRd"))(8)
-  
   d <- colSums(W)
-  
-  summary(d)
-  
-  quantile(d,0.99)
-  
+  plot.d <- d
   
   authors <- rownames(W)
-  
   hub.index <- which(d>225)
-  plot.d <- d
   plot.authors <- rep(NA,n)
   plot.authors[hub.index] <- authors[hub.index]
   
@@ -41,21 +33,36 @@ plotNetwork <- function(W, remove=0){
   V(g)$size <- sqrt(d)*0.5
   V(g)$label.cex <- 1
   
+  library(RColorBrewer)
+  colors <- colorRampPalette(brewer.pal(9,"YlOrRd"))(8)
   plot.color <- rep(colors[1],n)
   plot.color[d>quantile(d,0.5)] <- colors[2]
   plot.color[d>quantile(d,0.75)] <- colors[3]
   plot.color[d>quantile(d,0.9)] <- colors[4]
   plot.color[d>quantile(d,0.95)] <- colors[5]
   plot.color[d>quantile(d,0.98)] <- colors[6]
-  
-  
-  
   V(g)$color <- plot.color
   
+  set.seed(5) # 5
   plot(g, edge.width=sqrt(pmax(0, E(g)$weight-remove))-0.1, layout=layout_with_fr, margin=c(-0.2,-1,-0.2,-1))
   
+  return(g)
   #author.layout <- layout.fruchterman.reingold(g)
   #plot(g,layout=author.layout,vertex.label=NA,edge.width=0.8)
+}
+
+plotGroups <- function(g, weighted.cluster, remove=0){
+  K <- length(weighted.cluster)
+  library(Polychrome)
+  set.seed(3) # 3
+  colors = createPalette(K,  c("#ff0000", "#ffff00", "#0000ff"))
+  for(k in 1:K){
+    for (a in 1:length(weighted.cluster[[k]][,3])){
+      V(g)$color[as.numeric(weighted.cluster[[k]][a, 3])] <- colors[k]
+    }
+  }
+  set.seed(5) # 5
+  plot(g, edge.width=sqrt(pmax(0, E(g)$weight-remove))-0.1, layout=layout_with_fr, margin=c(-0.2,-1,-0.2,-1))
 }
 
 #pdf("AuthorWeightNet15Core.pdf",height=12,width=12)
--- StatisticianWeightedCore.R
+++ StatisticianWeightedCore.R
@@ -1,12 +1,5 @@
-require(doParallel)
-registerDoParallel(cores=6)
-
-rm(list=ls())
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-source("LeiFunc-Final.R")
-source("SimonFunk.R")
-source("RandomHoldout.R")
-source("ArashGen.R")
+rm(list=ls())
 source("AriHelpers.R")
 
 # load citations data
@@ -37,39 +30,33 @@ dim(W)
 plotNetwork(W)
 
 # Plot only edges with (weight > 1)
-plotNetwork(W, remove=1)
+g <- plotNetwork(W, remove=1)
 
 PSVD <- irlba(W,nv=100)
 
 plot(1:100,PSVD$d)
 
 # Stability selection, (Meinshausen & Peter Bühlmann, 2010)
-
-
-result <- foreach(k = 1:100, .packages='irlba')%dopar%{
-    set.seed(k)
-    random.est <- ECV.undirected.Rank.weighted(W,40,B=3,holdout.p=0.1,soft=FALSE,fast=TRUE)
-    tmp <- which.min(random.est$sse)
-}
-
+#result <- foreach(k = 1:100, .packages='irlba')%dopar%{
+#set.seed(k)
+random.est <- ECV.undirected.Rank.weighted(W,40,B=30,holdout.p=0.1,soft=FALSE,fast=TRUE)
+(tmp <- which.min(random.est$sse))
+#}
 
 # Count rank occurences
-SSE.K <- unlist(result)
-(occurences <- table(SSE.K))
-plot(occurences)
+#SSE.K <- unlist(result)
+#(occurences <- table(SSE.K))
+#plot(occurences)
 
 # Select rank
-(K <- as.numeric(names(which.max(occurences))))
+K <- tmp
+# (K <- as.numeric(names(which.max(occurences))))
 
-(authors <- rownames(W))
-length(authors)
+# Clustering with parameter
+tau.seq <- seq(0,3,by=0.1)
 
-
-h.seq <- seq(0,2,by=0.1)
-
-source("RandomHoldout-FinalSP_TestWeight.R")
 set.seed(500)
-#system.time(tune <- EdgeCV.REG.DC.Weight(W,h.seq,K=K,B=30,holdout.p=0.1,Arash=TRUE,fast=TRUE))
+#system.time(tune <- EdgeCV.REG.DC.Weight(W,h.seq,K=K,B=10,holdout.p=0.1,Arash=TRUE,fast=TRUE))
 #saveRDS(tune, file = "tune.Rda")
 tune <- readRDS("tune.Rda")
 
@@ -81,7 +68,7 @@ tune$gap.min.avg
 
 
 d <- colSums(W)
-W.reg <- W + h.seq[11]*mean(d)/n
+W.reg <- W + tau.seq[tune$gap.min.avg]*mean(d)/n
 
 d.reg <- colSums(W.reg)
 
@@ -102,25 +89,31 @@ norms <- apply(U,1,function(x)sqrt(sum(x^2)))
 U.norm <- U/norms
 
 set.seed(500)
-km1 <- kmeans(U.norm,centers=20,iter.max=500,nstart=500)
+km <- kmeans(U.norm,centers=K,iter.max=500,nstart=500)
 
 
-weighted.label1 <- km1$cluster
-weighted.cluster1 <- list()
+weighted.label <- km$cluster
+weighted.cluster <- list()
 for(k in 1:K){
-    tmp.authors <- authors[weighted.label1==k]
-    tmp.degrees <- d[weighted.label1==k]
+    print(tmp.positions <- which(weighted.label==k))
+    tmp.authors <- authors[weighted.label==k]
+    tmp.degrees <- d[weighted.label==k]
     tmp.index <- sort(tmp.degrees,decreasing=TRUE,index.return=TRUE)$ix
     
-    weighted.cluster1[[k]] <- cbind(tmp.authors[tmp.index],tmp.degrees[tmp.index])
+    weighted.cluster[[k]] <- cbind(tmp.authors[tmp.index],tmp.degrees[tmp.index],tmp.positions[tmp.index])
 }
+
+weighted.cluster
+
+plotGroups(g, weighted.cluster, remove=1.0)
 
 #save(km1,SSE.K,tune,file="CitationTune.Rda")
 #load("CitationTune.Rda")
 
+
 df <- rep("",K)
 for(k in 1:K){
-    df[k] <- paste(weighted.cluster1[[k]][1:7,1],collapse=", ")
+    df[k] <- paste(weighted.cluster[[k]][1:min(20, length(weighted.cluster[[k]][,1])-1),1],collapse=", ")
 }
 
 df <- data.frame(authors=df)
